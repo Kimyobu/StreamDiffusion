ca7dfe87bc5cfcd952d30e32696725ea932f3b83
Kimyobu <phanarin677@gmail.com>
2026-03-23 15:55:11 +0700

    Fix a bit on start.sh

--- demo/realtime-img2img/start.sh
+++ demo/realtime-img2img/start.sh
@@ -8,4 +8,4 @@ else
     echo -e "\033[1;31m\nfrontend build failed\n\033[0m" >&2  exit 1
 fi
 cd ../
-python3 main.py --port 7860 --host 0.0.0.0 --safety_checker false
+python3 main.py --port 7860 --host 0.0.0.0 --safety-checker false
